8839174e2c963014360eab790ad9b265dd920123
Bryan V <bryan@typocracy.net>
2015-03-14 14:27:52 -0500

    feature: triangle check/return

--- js/TriangleClassifier.js
+++ js/TriangleClassifier.js
@@ -1,6 +1,14 @@
-var TriangleClassifier = function() {
+var TriangleClassifier = function(sideAElement, sideBElement, sideCElement) {
 
-  this.classify = function() {
-    return "scalene";
+  this.classify = function(sideAElement, sideBElement, sideCElement) {
+    if( sideAElement == sideBElement && sideBElement == sideCElement){
+      return "equilateral";
+    } else if ( sideAElement == sideBElement || sideAElement == sideCElement ){
+      return "isosceles";
+    } else if ( sideAElement != sideBElement && sideBElement != sideCElement ) {
+      return "scalene";
+    } else {
+      return ArgumentError("Nope");
+    }
   };
 };
